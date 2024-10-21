373d7a4ae053eea784e505cfb585fe96c85fcc6c
Arsalan <arsalan.khan@tree-identity.com>
2024-10-22 00:26:30 +0530

    fix redirection issue

--- auth.ts
+++ auth.ts
@@ -53,10 +53,13 @@ export const {
 
       return session;
     },
-    // async redirect({ url, baseUrl }) {
-    //   // Ensure you are redirecting to a valid path
-    //   return baseUrl;
-    // },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
   },
   secret: process.env.AUTH_SECRET,
   // pages: {
